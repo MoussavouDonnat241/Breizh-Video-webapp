0183dc01e6e56c96504f568871fa283b5f8c8f1c
MoussavouDonnat241 <72128487+MoussavouDonnat241@users.noreply.github.com>
2020-12-22 16:29:42 +0100

    first commit

---
+++ src/test/java/com/BreizhVideo/BreizhVideowebapp/BreizhVideoWebAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.BreizhVideo.BreizhVideowebapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BreizhVideoWebAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
